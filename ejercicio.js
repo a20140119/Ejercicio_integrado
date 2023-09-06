857efee05cbe9a94165337981af1bfc5b548a5e5
a20140119 <131292531+a20140119@users.noreply.github.com>
2023-09-06 12:47:44 -0500

    Add files via upload

---
+++ ejercicio.js
@@ -0,0 +1,17 @@
+alert("Bienvenidos al restaurante. Su cuenta es...");
+//Ingresar Datos
+var entrada=parseInt(prompt("Ingrese el costo de la entrada"));
+var segundo=parseInt(prompt("Ingrese el costo del segundo"));
+var postre=parseInt(prompt("Ingrese el del postre"));
+var total= entrada + segundo + postre;
+var igv= total + (total*0.18);
+//Procesar Datos
+
+alert("Presione la tecla F12 para mostrar los resultados");
+//Mostrar Datos
+console.log("El costo de la entrada es: " + entrada);
+console.log("El costo del segundo es: " + segundo);
+console.log("El costo del postre es: " + postre);
+console.log("El costo total es: " + total);
+console.log("El costo total con IGV es: " + igv);
+
